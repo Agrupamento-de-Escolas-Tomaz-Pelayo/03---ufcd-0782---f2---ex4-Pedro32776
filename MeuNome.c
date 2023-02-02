9280193eda510424938cfeb946580f4100d8d17b
Pedro32776 <a32776@tomazpelayo.com>
2023-02-02 15:57:32 +0000

    f2.ex4

--- MeuNome.c
+++ MeuNome.c
@@ -3,7 +3,7 @@ int main ()
 {
 
 
-	printf ("Nome:Carlos Fonsenca\nData: 14 de setembro de 2007\nMorada:Santo Tirso");
+	printf ("Nome: Carlos Fonsenca\nData: 14 de setembro de 2007\nMorada: Santo Tirso");
 	return (0);
 }
 
